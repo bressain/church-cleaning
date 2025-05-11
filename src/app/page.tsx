882d353626c770ccdd21a6d228e9e5314f875281
Bressain Dinkelman <dinkelburt@gmail.com>
2025-05-10 18:44:25 -0600

    Add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return (
-    <div>
-      Hi
-    </div>
-  )
+	return (
+		<div>
+			<span>Hi</span>
+			<Button>Click me, I guess</Button>
+		</div>
+	)
 }
